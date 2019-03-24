038983468f14029e9c3971224e7bcac00c9e5b35
alvin amartya <alvinamartya1@gmail.com>
2019-03-24 20:27:35 +0700

    update last login

--- DIRENORD_15/MasterPage.master.cs
+++ DIRENORD_15/MasterPage.master.cs
@@ -4,78 +4,76 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.Configuration;
 
 public partial class MasterPage : System.Web.UI.MasterPage
 {
+    SqlConnection sqlCon = new SqlConnection(WebConfigurationManager.ConnectionStrings["direnord"].ConnectionString);
     protected void Page_Load(object sender, EventArgs e)
     {
         try
         {
-            if (Session["Status"].ToString() != null && Session["Rolename"].ToString() == "Admin")
+            if (Session["Status"].ToString() != null)
             {
-                //lbl_image.Text = "<img src='" + Session["Urlpict"].ToString() + "'alt = '...' class='img-circle profile_img' height='55' width='70'   >";
-                //lbl_image2.Text = "<img src='" + Session["Urlpict"].ToString() + "'alt = ''>";
-                txtTopHeaderRole.Text = "Admin";
-                lbl_name.Text = Session["Staffname"].ToString();
+                sqlCon.Open();
+                SqlCommand sqlCmd = new SqlCommand("sp_staff_get_data", sqlCon);
+                sqlCmd.Parameters.AddWithValue("@username", Session["Username"].ToString());
+                sqlCmd.CommandType = CommandType.StoredProcedure;
+                SqlDataReader reader = sqlCmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    txtTopHeaderRole.Text = reader[1].ToString();
+                    lbl_name.Text = reader[0].ToString();
+                    lblLastLogin.Text = "Login Terakhir: " + DateTime.Parse(reader[2].ToString()).ToString("dd MMMM yyyy, hh:mm") + " WIB";
+                    //lbl_image.Text = "<img src='" + Session["Urlpict"].ToString() + "'alt = '...' class='img-circle profile_img' height='55' width='70' >";
+                    //lbl_image2.Text = "<img src='" + Session["Urlpict"].ToString() + "'alt = ''>";
+                }
+                sqlCon.Close();
 
-                txtCardHeaderDate.Text = DateTime.Now.ToLongDateString().ToString();
+                if (Session["Rolename"].ToString() == "Admin")
+                {
+                    txtCardHeaderDate.Text = DateTime.Now.ToLongDateString().ToString();
+                    menu_admin.Visible = true;
+                    menu_verifier.Visible = false;
+                    menu_superadmin.Visible = false;
+                    menu_manager.Visible = false;
+                }
+                else if (Session["Rolename"].ToString() == "Verifier")
+                {
+                    menu_admin.Visible = false;
+                    menu_verifier.Visible = true;
+                    menu_superadmin.Visible = false;
+                    menu_manager.Visible = false;
+                }
+                else if (Session["Rolename"].ToString() == "Super Admin")
+                {
+                    txtCardHeaderDate.Text = DateTime.Now.ToLongDateString().ToString() + " | " + DateTime.Now.ToShortTimeString().ToString();
+                    menu_admin.Visible = false;
+                    menu_verifier.Visible = false;
+                    menu_superadmin.Visible = true;
+                    menu_manager.Visible = false;
 
-                menu_admin.Visible = true;
-                menu_verifier.Visible = false;
-                menu_superadmin.Visible = false;
-                menu_manager.Visible = false;
-            }
-            else if (Session["Status"].ToString() != null && Session["Rolename"].ToString() == "Verifier")
-            {
-                //lbl_image.Text = "<img src='" + Session["Urlpict"].ToString() + "'alt = '...' class='img-circle profile_img' height='55' width='70' >";
-                //lbl_image2.Text = "<img src='" + Session["Urlpict"].ToString() + "'alt = ''>";
-                lbl_name.Text = Session["Staffname"].ToString();
-                txtTopHeaderRole.Text = "Verifier";
-
-                menu_admin.Visible = false;
-                menu_verifier.Visible = true;
-                menu_superadmin.Visible = false;
-                menu_manager.Visible = false;
-
-            }
-            else if (Session["Status"].ToString() != null && Session["Rolename"].ToString() == "Super Admin")
-            {
-                //lbl_image.Text = "<img src='" + Session["Urlpict"].ToString() + "'alt = '...' class='img-circle profile_img' height='55' width='70' >";
-                //lbl_image2.Text = "<img src='" + Session["Urlpict"].ToString() + "'alt = ''>";
-                lbl_name.Text = Session["Staffname"].ToString();
-                txtCardHeaderDate.Text = DateTime.Now.ToLongDateString().ToString() + " | " + DateTime.Now.ToShortTimeString().ToString(); 
-
-                txtTopHeaderRole.Text = "Super Admin";
-                menu_admin.Visible = false;
-                menu_verifier.Visible = false;
-                menu_superadmin.Visible = true;
-                menu_manager.Visible = false;
-
-            }
-            else if (Session["Status"].ToString() != null && Session["Rolename"].ToString() == "Manajer")
-            {
-                //lbl_image.Text = "<img src='" + Session["Urlpict"].ToString() + "'alt = '...' class='img-circle profile_img' height='55' width='70' >";
-                //lbl_image2.Text = "<img src='" + Session["Urlpict"].ToString() + "'alt = ''>";
-                lbl_name.Text = Session["Staffname"].ToString();
-                txtTopHeaderRole.Text = "Manajer";
-                menu_admin.Visible = false;
-                menu_verifier.Visible = false;
-                menu_superadmin.Visible = false;
-                menu_manager.Visible = true;
-
+                }
+                else if (Session["Rolename"].ToString() == "Manajer")
+                {
+                    menu_admin.Visible = false;
+                    menu_verifier.Visible = false;
+                    menu_superadmin.Visible = false;
+                    menu_manager.Visible = true;
+                }
             }
             else
             {
                 Response.Redirect("/PageStaff/Login_Staff.aspx");
             }
-
         }
         catch
         {
             Response.Redirect("/PageStaff/Login_Staff.aspx");
         }
-
     }
 
-   
+
 }
