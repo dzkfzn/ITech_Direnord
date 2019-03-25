b6d1a3f459d30e3be0e77c6808884093f05a63cb
alvin amartya <alvinamartya1@gmail.com>
2019-03-25 09:26:55 +0700

    revisi last login

--- DIRENORD_15/PageStaff/Sign_Out_Staff.aspx.cs
+++ DIRENORD_15/PageStaff/Sign_Out_Staff.aspx.cs
@@ -5,11 +5,25 @@ using System.Web;
 using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.Configuration;
 
 public partial class PageStaff_Sign_Out_Staff : System.Web.UI.Page
 {
+    SqlConnection sqlCon = new SqlConnection(WebConfigurationManager.ConnectionStrings["direnord"].ConnectionString);
     protected void Page_Load(object sender, EventArgs e)
     {
+        if(Session["Username"].ToString() != null)
+        {
+            sqlCon.Open();
+            SqlCommand sqlCmd2 = new SqlCommand("sp_staff_last_login", sqlCon);
+            sqlCmd2.Parameters.AddWithValue("@staffUsername", Session["Username"].ToString());
+            sqlCmd2.CommandType = CommandType.StoredProcedure;
+            sqlCmd2.ExecuteNonQuery();
+            sqlCon.Close();
+        }
+
         Session.Clear();
         FormsAuthentication.SignOut();
         Response.Redirect("/PageStaff/Login_Staff.aspx");
